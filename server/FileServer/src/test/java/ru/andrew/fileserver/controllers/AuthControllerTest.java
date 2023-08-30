49d043aee2f1d1d53cac34c73dec2338de0a184e
Andrew <86295320+StellarLis@users.noreply.github.com>
2023-08-30 17:39:03 +0300

    Added AuthControllerTest class

--- server/FileServer/src/test/java/ru/andrew/fileserver/controllers/AuthControllerTest.java
+++ server/FileServer/src/test/java/ru/andrew/fileserver/controllers/AuthControllerTest.java
@@ -1,13 +1,15 @@
 package ru.andrew.fileserver.controllers;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -18,7 +20,7 @@ import ru.andrew.fileserver.entities.FileUser;
 import ru.andrew.fileserver.util.SessionFactoryImpl;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -33,6 +35,8 @@ class AuthControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
+    @Value("${JWT_PRIVATE_KEY}")
+    private String jwtKey;
 
     private Session session;
 
@@ -42,7 +46,7 @@ class AuthControllerTest {
     }
 
     @Test
-    void signUp_Returns200() throws Exception {
+    void signUp_successful_Returns200() throws Exception {
         FileUser fileUser = new FileUser("username", "password");
         when(fileUserDao.getCandidateByUsername(fileUser.getUsername(), session))
                 .thenReturn(null).thenReturn(fileUser);
@@ -50,5 +54,81 @@ class AuthControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(fileUser)))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void signUp_usernameExists_Returns400() throws Exception {
+        FileUser fileUser = new FileUser("username", "password");
+        when(fileUserDao.getCandidateByUsername(fileUser.getUsername(), session))
+                .thenReturn(fileUser);
+        mockMvc.perform(post("/auth/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(fileUser)))
+                .andExpect(status().is(400));
+    }
+
+    @Test
+    void login_successful_Returns200() throws Exception {
+        FileUser fileUser = new FileUser("username", "password");
+        String hash = BCrypt.withDefaults()
+                .hashToString(6, fileUser.getPassword().toCharArray());
+        FileUser candidate = new FileUser("username", hash);
+        when(fileUserDao.getCandidateByUsername(fileUser.getUsername(), session))
+                .thenReturn(candidate);
+        mockMvc.perform(post("/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(fileUser)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void login_userDoesNotExist_Returns400() throws Exception {
+        FileUser fileUser = new FileUser("username", "password");
+        when(fileUserDao.getCandidateByUsername(fileUser.getUsername(), session))
+                .thenReturn(null);
+        mockMvc.perform(post("/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(fileUser)))
+                .andExpect(status().is(400));
+    }
+
+    @Test
+    void login_incorrectPassword_Returns400() throws Exception {
+        FileUser fileUser = new FileUser("username", "password");
+        String hash = BCrypt.withDefaults()
+                .hashToString(6, "other_password".toCharArray());
+        FileUser candidate = new FileUser("username", hash);
+        when(fileUserDao.getCandidateByUsername(fileUser.getUsername(), session))
+                .thenReturn(candidate);
+        mockMvc.perform(post("/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(fileUser)))
+                .andExpect(status().is(400));
+    }
+
+    @Test
+    void authenticate_successful_Returns200() throws Exception {
+        Algorithm algorithm = Algorithm.HMAC256(jwtKey);
+        String token = JWT.create().withIssuer("auth0").sign(algorithm);
+        mockMvc.perform(get("/auth/authenticate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("authorization", "Bearer " + token))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void authenticate_userDoesNotHaveAToken_Returns401() throws Exception {
+        mockMvc.perform(get("/auth/authenticate")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(401));
+    }
+
+    @Test
+    void authenticate_userHasInvalidToken_Returns401() throws Exception {
+        String token = "random_incorrect_token";
+        mockMvc.perform(get("/auth/authenticate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("authorization", "Bearer " + token))
+                .andExpect(status().is(401));
     }
 }
