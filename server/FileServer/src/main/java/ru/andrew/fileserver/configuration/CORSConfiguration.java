849795044abeca0694f2a2f592e124208e775fa8
Andrew <andrey13788822@gmail.com>
2024-01-18 18:09:48 +0300

    Hotfix

--- server/FileServer/src/main/java/ru/andrew/fileserver/configuration/CORSConfiguration.java
+++ server/FileServer/src/main/java/ru/andrew/fileserver/configuration/CORSConfiguration.java
@@ -8,9 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CORSConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "DELETE", "PUT")
-                .allowedHeaders("*");
+        registry.addMapping("/**");
     }
 }
